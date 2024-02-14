04548dd81a98132c091eaa8e19c85a8940f32abb
farhanak <farhana.khashiya@gmail.com>
2024-02-14 13:51:56 +0100

    header is responsive

--- my-portfolio/src/components/Header/Header.jsx
+++ my-portfolio/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import profilepic from "../assets/linkedinprofile.jpeg";
 const Header = () => {
+  const [hamburger, setHamburger] = useState(false);
   return (
-    <Container>
+    <Container hamburger={hamburger}>
       <Logo>
         <img src={profilepic} alt="" />
         <h1>Portfolio</h1>
       </Logo>
-      <Navbar>
+      <Navbar hamburger={hamburger}>
         <span>
           <a href="#home">Home</a>
         </span>
@@ -22,6 +23,9 @@ const Header = () => {
           <a href="#home">Contact</a>
         </span>
       </Navbar>
+      <div className="menu" onClick={() => setHamburger(!hamburger)}>
+        <div className="hamburger"></div>
+      </div>
     </Container>
   );
 };
@@ -36,14 +40,58 @@ const Container = styled.div`
   width: 80%;
   margin: 0 auto;
   padding: 1.5rem 0;
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+  .menu {
+    display: none;
+  }
+
+  @media (max-width: 640px) {
+    .menu {
+      width: 40px;
+      height: 40px;
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0.5rem;
+      z-index: 100;
+      .hamburger {
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        background-color: ${(props) =>
+          props.hamburger ? "transparent" : "#fff"};
+        transition: all 400ms ease-in-out;
+        &::before,
+        &::after {
+          content: "";
+          width: 100%;
+          height: 2px;
+          background-color: #fff;
+          position: absolute;
+        }
+
+        &::before {
+          transform: ${(props) =>
+            props.hamburger ? "rotate(45deg)" : "translateY(10px)"};
+          transition: all 400ms ease-in-out;
+        }
+
+        &::after {
+          transform: ${(props) =>
+            props.hamburger ? "rotate(-45deg)" : "translateY(-10px)"};
+          transition: all 400ms ease-in-out;
+        }
+      }
+    }
+  }
 `;
 const Logo = styled.div`
-  max-width: 1280px;
   display: flex;
   align-items: center;
-  width: 80%;
-  margin: 0 auto;
-  padding: 1.5rem 0;
+
   gap: 1rem;
   img {
     width: 8rem;
@@ -57,6 +105,25 @@ const Logo = styled.div`
 `;
 
 const Navbar = styled.div`
+  @media (max-width: 640px) {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    background-color: #257060;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    gap: 2rem;
+    height: ${(props) => (props.hamburger ? "100vh" : 0)};
+    transition: height 400ms ease-in-out;
+    overflow: hidden;
+    z-index: 99;
+  }
+
   span {
     margin-left: 1rem;
 
